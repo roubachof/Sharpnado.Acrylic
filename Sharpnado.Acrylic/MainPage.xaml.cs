dd713751982470a9b4d3beecaf2adf580198c2c2
roubachof <jm.alfonsi@gmail.com>
2020-04-01 19:55:48 +0200

    First commit

---
+++ Sharpnado.Acrylic/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace Sharpnado.Acrylic
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            SetValue(NavigationPage.HasNavigationBarProperty, false);
+            InitializeComponent();
+        }
+    }
+}
